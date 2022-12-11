56aa72ee73e582a0f5e37d146cfb49df26cc2f3e
Andreas Kurth <adk@lowrisc.org>
2022-12-11 18:38:01 +0100

    [demo] Tweak script to pause and handle Ctrl-C
    Signed-off-by: Andreas Kurth <adk@lowrisc.org>

--- run_demo.sh
+++ run_demo.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/env bash
 
+trap handle_ctrl_c INT
+running=true
+
+function handle_ctrl_c() {
+    running=false
+}
+
 if test -z "$BAZEL"; then
     BAZEL=bazel
 fi
 
 if test -z "$TARGET"; then
     TARGET=fpga_cw310_rom
+fi
+
+if test -z "$PAUSE"; then
+    PAUSE=7
 fi
 
 run_test() {
@@ -15,7 +26,12 @@ run_test() {
         //sw/device/tests:${1}_${TARGET}
 }
 
-while true; do
-    run_test aes_smoketest
-    run_test otbn_smoketest
- done
+while $running; do
+    for t in aes_smoketest otbn_smoketest; do
+        run_test $t
+        if ! $running; then
+            break
+        fi
+        sleep $PAUSE
+    done
+done
